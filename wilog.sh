02fb2a859541ee5f85061cb500722e60ab5bd64f
JabriMuhi <73785841+JabriMuhi@users.noreply.github.com>
2020-12-11 04:04:55 +0300

    Delete wilog.sh

--- wilog.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-log_file="/var/log/anaconda/X.log"
-awk '$3 == "(WW)" {print}' $log_file | sed 's/(WW)/Warning:/' > full.log
-awk '$3 == "(II)" {print}' $log_file | sed 's/(II)/Information:/' >> full.log
